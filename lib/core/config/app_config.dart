488516428ede2be929835ef079eef2f17e235bad
Adithya Firmansyah Putra <aditputrafirmansyah@gmail.com>
2026-03-11 04:35:11 +0700

    feat(scaffold): feature-first clean arch structure + env config
    POW-7: folder structure, deps (powersync_core, supabase_flutter, riverpod,
    image_picker, uuid, flutter_dotenv), AppConfig, .env.example, updated main.dart.

---
+++ lib/core/config/app_config.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class AppConfig {
+  AppConfig._();
+
+  static String get supabaseUrl => dotenv.env['SUPABASE_URL']!;
+  static String get supabaseAnonKey => dotenv.env['SUPABASE_ANON_KEY']!;
+  static String get supabaseServiceRoleKey =>
+      dotenv.env['SUPABASE_SERVICE_ROLE_KEY']!;
+  static String get powerSyncUrl => dotenv.env['POWERSYNC_URL']!;
+}
